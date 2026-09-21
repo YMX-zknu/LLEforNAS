95f5bf88ed29deae8c0a6750da7512ab8a6dd2b4
YMX <39787386+YMX-zknu@users.noreply.github.com>
2026-09-21 15:25:56 +0800

    Include the dataset package in distributions

---
+++ src/lnas/data/__init__.py
@@ -0,0 +1,10 @@
+from .registry import DatasetBundle, DatasetSpec, SPECS, build_datasets, build_loaders, prepare_batch
+
+__all__ = [
+    "DatasetBundle",
+    "DatasetSpec",
+    "SPECS",
+    "build_datasets",
+    "build_loaders",
+    "prepare_batch",
+]
